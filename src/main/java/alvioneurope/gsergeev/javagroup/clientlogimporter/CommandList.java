94d426394b9093a8edfd6b52dc0412c3f51886b7
Alexey Batrachenko <zzzaqxswcdevfrbgt98@gmail.com>
2015-11-17 00:50:33 +0300

    kintaro18 issues 1-5

--- src/main/java/alvioneurope/gsergeev/javagroup/clientlogimporter/CommandList.java
+++ src/main/java/alvioneurope/gsergeev/javagroup/clientlogimporter/CommandList.java
@@ -1,5 +1,7 @@
 package alvioneurope.gsergeev.javagroup.clientlogimporter;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -7,15 +9,29 @@ import java.util.List;
  */
 public class CommandList {
     private List <Command> commandList;
-    public CommandList () {
-
+    public List<Command> getCommandList(){
+    	return commandList;
     }
-
+    
+    public void setCommandList(List<Command> commandList){
+    	this.commandList = commandList;
+    }
+    
+    public CommandList () {
+    	commandList = new ArrayList<Command>();
+    }
+    
     public void addCommand (Command command) {
-
+    	commandList.add(command);
     }
 
     public Command findCommandById(int id) {
-        return null;
+    	Command foundCommand = null;
+    	for(Iterator<Command> iterator = commandList.iterator(); iterator.hasNext();){
+    		if (iterator.next().getCommandId()==id) {
+    			foundCommand = iterator.next();
+    		}
+    	}
+        return foundCommand;
     }
 }
--- src/main/java/alvioneurope/gsergeev/javagroup/clientlogimporter/ExecutedCommand.java
+++ src/main/java/alvioneurope/gsergeev/javagroup/clientlogimporter/ExecutedCommand.java
@@ -11,5 +11,76 @@ public class ExecutedCommand {
     private String operationId;
     private int responseLength;
     private int execTime;
-
+    
+    public ExecutedCommand(String executedCommand){
+    	int commandId = 0;
+    	String commandName = null;
+    	command = new Command(commandId,commandName);
+    	String splitExecutedCommand[] = executedCommand.split("  ");
+    	for (int i=0; i<splitExecutedCommand.length; i++){
+    		if (splitExecutedCommand[i].compareTo("RequestId")== 0) requestId = splitExecutedCommand[i+1].replaceAll("\"", "");
+    		if (splitExecutedCommand[i].compareTo("commandId")== 0) command.setCommandId(Integer.parseInt(splitExecutedCommand[i+1]));
+    		if (splitExecutedCommand[i].compareTo("commandName")== 0) command.setCommandName(splitExecutedCommand[i+1].replaceAll("\"", ""));
+    		if (splitExecutedCommand[i].compareTo("timeout")== 0) timeout = Integer.parseInt(splitExecutedCommand[i+1]);
+    		if (splitExecutedCommand[i].compareTo("requestLength")== 0) requestLength = Integer.parseInt(splitExecutedCommand[i+1]);
+    		if (splitExecutedCommand[i].compareTo("operationId")== 0) operationId = splitExecutedCommand[i+1].replaceAll("\"", "");
+    		if (splitExecutedCommand[i].compareTo("responseLength")== 0) responseLength = Integer.parseInt(splitExecutedCommand[i+1]);
+    		if (splitExecutedCommand[i].compareTo("execTime")== 0) execTime = Integer.parseInt(splitExecutedCommand[i+1].replaceAll(" ", ""));
+    	}
+    }
+    public String getRequestId(){
+    	return requestId;
+    }
+    
+    public Command getCommand(){
+    	return command;
+    }
+    
+    public int getTimeout(){
+    	return timeout;
+    }
+    
+    public int getRequestLength(){
+    	return requestLength;
+    }
+    
+    public String getOperationId(){
+    	return operationId;
+    }
+    
+    public int getResponseLength(){
+    	return responseLength;
+    }
+    
+    public int getExecTime(){
+    	return execTime;
+    }
+    
+    public void setCommand(Command command){
+    	this.command = command;
+    }
+    
+    public void setRequestId(String requestId){
+    	this.requestId = requestId;
+    }
+    
+    public void setTimeout(int timeOut){
+    	this.timeout = timeOut;
+    }
+    
+    public void setRequestLength(int requestLength){
+    	this.requestLength = requestLength;
+    }
+    
+    public void setOperationId(String operationId){
+    	this.operationId = operationId;
+    }
+    
+    public void setResponseLength(int responseLength){
+    	this.responseLength = responseLength;
+    }
+    
+    public void setExecTime(int execTime){
+    	this.execTime = execTime;
+    }
 }
