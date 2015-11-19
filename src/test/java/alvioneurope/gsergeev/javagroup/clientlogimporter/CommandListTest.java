053b4f4d7fa5b1fd60e18f99d259493bd9a926be
Alexey Batrachenko <zzzaqxswcdevfrbgt98@gmail.com>
2015-11-19 10:40:45 +0300

    issue 2

--- src/test/java/alvioneurope/gsergeev/javagroup/clientlogimporter/CommandListTest.java
+++ src/test/java/alvioneurope/gsergeev/javagroup/clientlogimporter/CommandListTest.java
@@ -6,18 +6,20 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * Created by Алексей on 18.11.2015.
+ * Created by kintaro18 on 18.11.2015.
  */
 public class CommandListTest {
     private CommandList testCommandList;
     private Command testCommand1;
     private Command testCommand2;
+    private Command testCommand3;
 
     @Before
     public void setUp(){
         testCommandList = new CommandList();
         testCommand1 = new Command(1,"Command1");
         testCommand2 = new Command(2,"Command2");
+        testCommand3 = new Command(3, "Command3");
     }
 
     @Test
@@ -33,9 +35,9 @@ public class CommandListTest {
 
     @Test
     public void testFindCommandById() throws Exception {
-        assertEquals(null,testCommandList.findCommandById(testCommand1.getCommandId()));
         testCommandList.addCommand(testCommand1);
         testCommandList.addCommand(testCommand2);
+        assertNull(testCommandList.findCommandById(testCommand3.getCommandId()));
         assertEquals(testCommand1, testCommandList.findCommandById(testCommand1.getCommandId()));
         assertEquals(testCommand2, testCommandList.findCommandById(testCommand2.getCommandId()));
     }
